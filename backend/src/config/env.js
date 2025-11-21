c5c6db2e277186b4a47324d3c9920da6b4416623
Krithikakannan <krithika.kiki0123@gmail.com>
2025-11-21 05:41:26 +0530

    chore: force remove all files

--- backend/src/config/env.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-console.log("🧩 Loaded MONGO_URI from env:", process.env.MONGO_URI);
-
-export const PORT = process.env.PORT || 5000;
-export const MONGO_URI = process.env.MONGO_URI;
-export const JWT_SECRET = process.env.JWT_SECRET || "secret";
